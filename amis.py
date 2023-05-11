e3033872e60698219019731f4402372b01f02a67
Jakob Föger <60188304+drvolcano@users.noreply.github.com>
2023-05-11 23:55:52 +0200

    Update amis.py

--- amis.py
+++ amis.py
@@ -8,6 +8,8 @@ sampledata = [104, 95, 95, 104, 115, 240, 91, 0, 0, 0, 0, 45, 76, 1, 14, 12, 0, 
 key = "32D13550827108D59C460D6A9DBAD7BE"
 keybytes = bytes(bytearray.fromhex(key))
 
+registers = bytearray()
+
 serial = Serial()
 serial.port = "/dev/ttyUSB1"
 serial.baudrate = 9600
@@ -17,82 +19,143 @@ serial.bytesize = 8
 serial.timeout = 1
 serial.open()
 
-reading = False
-buffer = bytes()
-length = 0
+lock = Lock()
 
-while True:
-    byte = serial.read(1)
+def amisreader():
+    reading = False
+    buffer = bytes()
+    length = 0
 
-    if not reading and byte == b'\x68':
-        buffer = []
-        length = 0
-        reading = True
 
-    if reading:
-        buffer.extend(byte)
+    while True:
+        byte = serial.read(1)
 
-        if len(buffer) == 4:
-            if buffer[0] == 0x68 and buffer[1] == buffer[2] and buffer[3] == 0x68:
-                length = buffer[1]
-            else:
+        if not reading and byte == b'\x68':
+            buffer = []
+            length = 0
+            reading = True
+
+        if reading:
+            buffer.extend(byte)
+
+            if len(buffer) == 4:
+                if buffer[0] == 0x68 and buffer[1] == buffer[2] and buffer[3] == 0x68:
+                    length = buffer[1]
+                else:
+                    reading = False
+
+            if len(buffer) == (4 + length + 2):
+                #print(str(bytes(buffer)))
+                content = buffer[4:-2]
                 reading = False
+                c = content[0]
+                a = content[1]
+                ci = content[2]
+                identification =  content[3:7]
+                manufacturer = content[7:9]
+                version = content[9]
+                devType = content[10]
+                access = content[11]
+                status = content[12]
+                configuration = content[13:15]
+                encrypted = content[15:]
 
-        if len(buffer) == (4 + length + 2):
-            #print(str(bytes(buffer)))
-            content = buffer[4:-2]
-            reading = False
-            c = content[0]
-            a = content[1]
-            ci = content[2]
-            identification =  content[3:7]
-            manufacturer = content[7:9]
-            version = content[9]
-            devType = content[10]
-            access = content[11]
-            status = content[12]
-            configuration = content[13:15]
-            encrypted = content[15:]
+                iv = []
+                iv.extend(manufacturer)
+                iv.extend(identification)
+                iv.append(version)
+                iv.append(devType)
 
-            iv = []
-            iv.extend(manufacturer)
-            iv.extend(identification)
-            iv.append(version)
-            iv.append(devType)
+                for x in range(8):
+                    iv.append(access)
 
-            for x in range(8):
-                iv.append(access)
+                ivbytes = bytes(iv)
 
-            ivbytes = bytes(iv)
+                obj2 = AES.new(keybytes, AES.MODE_CBC, ivbytes)
+                decrypted = obj2.decrypt(bytes(encrypted))
 
-            obj2 = AES.new(keybytes, AES.MODE_CBC, ivbytes)
-            decrypted = obj2.decrypt(bytes(encrypted))
+                ary = bytearray(decrypted)
 
-            ary = bytearray(decrypted)
+                # b066D = int.from_bytes(ary[4:4+6],'little')#Datum M-Bus Format
+                # b0403 = int.from_bytes(ary[12:12+4],'little')#1.8.0 Wirk Bezug Wh
+                # b04833C = int.from_bytes(ary[19:19+4],'little')#2.8.0 Wirk Einspeis Wh
+                # b8410FB8273 = int.from_bytes(ary[28:28+4],'little')#3.8.1 Blind + varh
+                # b8410FB82F33C = int.from_bytes(ary[38:38+4],'little')#4.8.1 Blind - varh
+                # b042B = int.from_bytes(ary[44:44+4],'little')#Wirkleistung Bezug W
+                # b04AB3C = int.from_bytes(ary[51:51+4],'little')#Wirkleistung Einspeis W
+                # b04FB14 = int.from_bytes(ary[58:58+4],'little')#3.7.0 Blindleistung + W
+                # b04FB943C = int.from_bytes(ary[66:66+4],'little')#4.7.0 Blindleistung - W
+                # b0483FF04 = int.from_bytes(ary[74:74+4],'little')#1.128.0 Inkassozählwerk Wh
 
-            b066D = int.from_bytes(ary[4:4+6],'little')#Datum M-Bus Format
-            b0403 = int.from_bytes(ary[12:12+4],'little')#1.8.0 Wirk Bezug Wh
-            b04833C = int.from_bytes(ary[19:19+4],'little')#2.8.0 Wirk Einspeis Wh
-            b8410FB8273 = int.from_bytes(ary[28:28+4],'little')#3.8.1 Blind + varh
-            b8410FB82F33C = int.from_bytes(ary[38:38+4],'little')#4.8.1 Blind - varh
-            b042B = int.from_bytes(ary[44:44+4],'little')#Wirkleistung Bezug W
-            b04AB3C = int.from_bytes(ary[51:51+4],'little')#Wirkleistung Einspeis W
-            b04FB14 = int.from_bytes(ary[58:58+4],'little')#3.7.0 Blindleistung + W
-            b04FB943C = int.from_bytes(ary[66:66+4],'little')#4.7.0 Blindleistung - W
-            b0483FF04 = int.from_bytes(ary[74:74+4],'little')#1.128.0 Inkassozählwerk Wh
+                global registers 
+                registers = ary[4:4+6][::-1]+ ary[12:12+4][::-1]+ ary[19:19+4][::-1] + ary[28:28+4][::-1] + ary[38:38+4][::-1]+ ary[44:44+4][::-1] + ary[51:51+4][::-1] + ary[58:58+4][::-1]+ ary[66:66+4][::-1] + ary[74:74+4][::-1]
+   
+        if byte == b'\x16':
+            serial.write(b'\xe5')
+        
+        
+def client_handler(connection):
+    while True:
 
-            print('#############################')
-            print(str(a))
-            print(str(b))
-            print(str(c))
-            print(str(d))
-            print(str(e))
-            print(str(f))
-            print(str(g))
-            print(str(h))
-            print(str(i))
+        try:
+            tcp_request = connection.recv(1024 )
+        except BlockingIOError:
+            return   # socket is open and reading from it would block
+        except ConnectionResetError:
+            return   # socket was closed for some other reason
+        except Exception as e:
+            return
 
-    if byte == b'\x16':
-        print("response")
+       #print("request(TCP): " + str(tcp_request))
 
-        serial.write(b'\xe5')
+        if not tcp_request:
+            return
+
+        if len(tcp_request) >6:
+            lock.acquire()
+            length= int.from_bytes( tcp_request[4:6],'big')
+            rtu_request = tcp_request[6:] 
+            address = rtu_request[0]
+            code = rtu_request[1]
+
+            print(str(tcp_request))
+
+            register = int.from_bytes( rtu_request[2:4],'big')
+            length = int.from_bytes( rtu_request[4:6],'big')
+
+
+            data = registers[register*2: (register+length)*2]
+
+            rtu_response = rtu_request[0:2] +  (len(data)).to_bytes(1,'big') + data
+        
+    
+         
+      
+            tcp_response = tcp_request[0:4] + (len(rtu_response)).to_bytes(2,'big') + rtu_response
+            connection.send(tcp_response)
+            print(str(tcp_response))
+            lock.release()
+
+def accept_connections(ServerSocket):
+    Client, address = ServerSocket.accept()
+    print('Connected to: ' + address[0] + ':' + str(address[1]))
+    start_new_thread(client_handler, (Client, ))
+
+def start_server(host, port):
+    ServerSocket = socket.socket()
+    ServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    try:
+        ServerSocket.bind((host, port))
+    except socket.error as e:
+        print(str(e))
+    print(f'Server is listing on the port {port}...')
+    ServerSocket.listen()
+
+    while True:
+        accept_connections(ServerSocket)
+
+
+
+start_new_thread(amisreader, ())
+
+start_server("", 1503)
